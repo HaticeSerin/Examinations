64c50e78b37dddda4f6264b071ef58a851413942
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-20 13:33:27 +0300

    Hatice Serin
    @themanoftalent

--- union.c
+++ union.c
@@ -1,23 +1,28 @@
 // This is about a union
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-
-union bilgi{
-    int yas;
-    char isim[20];
-    char soyisim[20];
+#include"stdio.h"
+#include"stdlib.h"
+#include"string.h"
+union student{
+	int numara;
+	float sinavnotu;
+	char isim[20];	
 };
-
 int main(){
-    union bilgi veri;
-    veri.yas = 36;
-    strcpy(veri.isim,"akif");
-    strcpy(veri.soyisim,"cifci");  
-    printf("%d",veri.yas);
-    printf("%s",veri.isim);
-    printf("%s",veri.soyisim);
-    return 0;
+	union student veri;
+	basla:
+	printf("Enter your name:");
+	scanf("%s",veri.isim);
+	printf("Enter your number: ");
+	scanf("%d",&veri.numara);
+	printf("Enter your exam grade: ");
+	scanf("%f",&veri.sinavnotu);
+	if(veri.sinavnotu<50){
+		printf("Your exam grade %.2f .You failed the exam.\n",veri.sinavnotu);
+	}
+	else{
+		printf("Your exam grade %.2f .You passed the exam.\n",veri.sinavnotu);
+	}
+	goto basla;
+	return 0;
 }
-
